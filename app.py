9004c9fa19171f0acf600b41948dec5356391c03
Santiago Ramos <78809453+samidhdk@users.noreply.github.com>
2023-12-04 20:53:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from Rumble import Rumble
-from bson import json_util
 from fastapi import FastAPI, Path, Query, HTTPException, status
-from fastapi.responses import RedirectResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import RedirectResponse, JSONResponse
 from pydantic import BaseModel
 import uvicorn
-import pymongo
 
 uri = "mongodb+srv://..."
 app = FastAPI()
@@ -26,32 +25,29 @@ class Card(BaseModel):
     description: str
     traits: list[str]
 
-
-def json_encoder(obj):
-    if isinstance(obj, pymongo.objectid.ObjectId):
-        return str(obj)
-    raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
-
-
 @app.get("/", include_in_schema=False)
 async def redirect():
     response = RedirectResponse(url="/docs")
     return response
 
-
 @app.get("/units")
 async def get_all_units():
-    cursor = collection.find({}, {'_id': 0})
-    json_results = json_util.dumps(cursor, default=json_encoder)
-    return json_results
+    cursor = list(collection.find({}, {'_id': 0}))
+    if cursor:
+        return JSONResponse(content=jsonable_encoder(cursor))
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Units not found")
 
 
 @app.get("/units/{unit_id}")
 async def get_unit_by_id(unit_id: int = Path(..., description="The ID of the unit you would like to retrieve")):
     cursor = collection.find_one({'id': unit_id}, {'_id': 0})
     if cursor:
-        json_results = json_util.dumps(cursor, default=json_encoder)
-        return json_results
+        #json_results = json_util.dumps(cursor, default=json_encoder)
+        #return json_results
+        json_compatible_item_data = jsonable_encoder(cursor)
+        return JSONResponse(content=json_compatible_item_data)
+
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unit id not found")
 
@@ -63,10 +59,9 @@ async def get_units_by_faction(
     factions = {"horde", "blackrock", "alliance", "beast", "undead"}
     if unit_faction in factions:
         unit_faction = unit_faction.title()
-        cursor = collection.find({'faction': unit_faction}, {'_id': 0})
+        cursor = list(collection.find({'faction': unit_faction}, {'_id': 0}))
         if cursor:
-            json_results = json_util.dumps(cursor, default=json_encoder)
-            return json_results
+            return JSONResponse(content=jsonable_encoder(cursor))
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unit faction not found")
 
@@ -77,10 +72,9 @@ async def get_units_by_type(unit_type: str):
     types = {"troop", "spell", "leader"}
     if unit_type in types:
         unit_type = unit_type.title()
-        cursor = collection.find({'type': unit_type}, {'_id': 0})
+        cursor = list(collection.find({'type': unit_type}, {'_id': 0}))
         if cursor:
-            json_results = json_util.dumps(cursor, default=json_encoder)
-            return json_results
+            return JSONResponse(content=jsonable_encoder(cursor))
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unit type not found")
 
@@ -88,32 +82,28 @@ async def get_units_by_type(unit_type: str):
 @app.get("/units/cost/{unit_cost}")
 async def get_units_by_cost(unit_cost: int = Path(..., description="The cost of the units you want to retrieve")):
     if 1 <= unit_cost <= 6:
-        cursor = collection.find({'cost': unit_cost}, {'_id': 0})
+        cursor = list(collection.find({'cost': unit_cost}, {'_id': 0}))
         if cursor:
-            json_results = json_util.dumps(cursor, default=json_encoder)
-            return json_results
+            return JSONResponse(content=jsonable_encoder(cursor))
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unit cost not found")
 
 
 @app.get("/units/traits/{unit_traits}")
 async def get_units_by_traits(unit_traits: str = Path(..., description="The trait of the units you want to retrieve"),
-                              s1: str = Query(None),
-                              s2: str = Query(None),
-                              s3: str = Query(None)):
+                              s1: str = Query(None, alias="First optional subtrait"),
+                              s2: str = Query(None, alias="Second optional subtrait"),
+                              s3: str = Query(None, alias="Third optional subtrait"),):
     subtraits = [trait for trait in [s1, s2, s3] if trait is not None]
 
     query = {"traits": {"$all": [unit_traits, *subtraits]}} if subtraits else {"traits": unit_traits}
 
-    cursor = collection.find(query, {'_id': 0})
+    cursor = list(collection.find(query, {'_id': 0}))
     if cursor:
-        json_results = json_util.dumps(cursor, default=json_encoder)
-        if json_results != "[]":
-            return json_results
-        else:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unit traits not found")
+        return JSONResponse(content=jsonable_encoder(cursor))
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unit traits not found")
+
 
 
 @app.post("/add_unit/{new_card_id}}", include_in_schema=False)  # POST - METHOD to add a new unit
